c8cb13e6abc02cd976d5b2c90cd94c8e92123fd5
f-takeda-kaho <kaho@freemind.co.jp>
2025-12-12 10:34:18 +0900

    cognitoと連携_.env記載まで

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  
+  future: {
+    compatibilityVersion: 4,
+  },
+  
   nitro: {
     inlineDynamicImports: true,
     preset: 'aws-lambda',
@@ -9,5 +14,31 @@ export default defineNuxtConfig({
     output: {
       publicDir: '.output/server',
     }
-  }
+  },
+  
+  modules: [
+    '@sidebase/nuxt-auth'
+  ],
+  
+  runtimeConfig: {
+    authSecret: process.env.AUTH_SECRET,
+    cognito: {
+      clientId: process.env.COGNITO_CLIENT_ID,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      issuer: process.env.COGNITO_ISSUER, // https://cognito-idp.{region}.amazonaws.com/{userPoolId}
+    },
+    public: {
+      auth: {
+        baseURL: process.env.AUTH_ORIGIN || 'http://localhost:3000',
+        provider: {
+          type: 'authjs'
+        },
+        sessionRefresh: {
+          enablePeriodically: true,
+          enableOnWindowFocus: true,
+        }
+      },
+      authOrigin: process.env.AUTH_ORIGIN || 'http://localhost:3000',
+    }
+  },
 })
